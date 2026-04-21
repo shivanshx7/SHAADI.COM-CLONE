6125a2361598dfe2608ac6fd03d545e8a158c466
Jash <sankajash@gmail.com>
2026-04-21 08:03:43 +0530

    Added Navbar and basic hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import styles from './Hero.module.css'
+
+export default function Hero(){
+    return( 
+       <div className={styles.main}>
+        <section className={styles.content}>
+            <h1 className={styles.title}>
+                Find your forever ❤️
+            </h1>
+            <p className={styles.subtitle}>
+                Discover a world beyond matrimony
+            </p>
+            <button className={styles.matchButton}>
+                Find Your Match
+            </button>
+        </section>
+        
+        <div className={styles.statsBar}>
+            <p>
+                #1 Matchmaking Service &nbsp; | &nbsp; ⭐️⭐️⭐️⭐️⭐️ Ratings on Playstore by 2.4 lakh users &nbsp; | &nbsp; 80 Lakh Success Stories
+            </p>
+        </div>
+       </div>
+    )
+}
